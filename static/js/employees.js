af4fac4c307ff6b0d861bbfd34dfec5215530747
Daniel <homerion@gmail.com>
2018-07-11 16:31:19 -0400

    Table is responsive to variable JSON file

--- static/js/employees.js
+++ static/js/employees.js
@@ -3,19 +3,38 @@ $("#home_link").removeClass("active");
 $("#emp_link").addClass("active");
 }, 5)
 
-
-
+// function onlyUnique(value, index, self) {
+//     return self.indexOf(value) === index;
+// }
+//
+// function inArray() {
+//
+// }
 
 function enumerate(response) {
-  console.log(response.employees);
+
+// working to have all unique keys for each employee:
+// var titles=["Number"];
+// console.log(Object.keys(response.employees[0]));
+// var new_titles = Object.keys(response.employees[0]);
+// console.log(new_titles);
+// var newest_titles = titles.concat(new_titles);
+// console.log(newest_titles);
+
+  var row = $("<tr></tr>");
+  for (titles in response.employees[0]){
+    var col = $("<th scope='col'></th>").html(titles);
+    row.append(col);
+
+  }
+  $("#table-head").append(row);
   for (i=0; i<response.employees.length;i++){
     var row = $("<tr></tr>");
-    var name = $("<td></td>").html(response.employees[i].name);
-    var title = $("<td></td>").html(response.employees[i].title);
-    var salary = $("<td></td>").html(response.employees[i].salary);
-    var attendance = $("<td></td>").text(response.employees[i].attendance);
-    row.append(name,title,salary,attendance);
-    $("#emp_table").append(row);
+    for (val in response.employees[i]){
+      var col = $("<td></td>").text(response.employees[i][val]);
+      row.append(col);
+  }
+    $("#table-body").append(row);
   }
 }
 
